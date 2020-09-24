fcd4a2b0678bb2da697efc8a660828589441caa4
rottedBen <71821951+rottedBen@users.noreply.github.com>
2020-09-24 16:30:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ locale.setlocale(locale.LC_ALL, 'en_US')
 
 re_4chan = re.compile(r'^rot |rot$| rot |rotten|rotting')
 
+# scraps the github project to get those sweet memes. Will chose one randomly and send it.
 def get_url_meme():
     contents=requests.get("https://api.github.com/repos/rottenswap/rottenswap.org-website/contents/docs/memes").json()
     potential_memes = []
@@ -25,12 +26,13 @@ def bop(bot, update):
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
-
+# tutorial on how to increase the slippage.
 def howtoslippage(bot, update):
     url = "https://i.imgur.com/TVFhZML.png"
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
+# Get the supply cache from etherscan. Uses the ETH_API_KEY passed as an env variable.
 def getSupplyCap(bot, update):
     rotAddr = 'https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0xD04785C4d8195e4A54d9dEc3a9043872875ae9E2&apikey=' + etherscan_api_key
     maggotAddr = 'https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0x163c754eF4D9C03Fc7Fa9cf6Dd43bFc760E6Ce89&apikey=' + etherscan_api_key
@@ -43,6 +45,7 @@ def getSupplyCap(bot, update):
     chat_id = update.message.chat_id
     bot.send_message(chat_id=chat_id, text=message, parse_mode='html')
 
+# scraps /biz/ and returns a list of tuple (threadId, body, title) of threads matching the regex ^rot |rot$| rot |rotten|rotting
 def getBizThreads():
     url = 'https://a.4cdn.org/biz/catalog.json'
     json = requests.get(url).json()
@@ -69,6 +72,7 @@ def getBizThreads():
                     threadsIds.append((id, com, sub))
     return threadsIds
 
+# sends the current biz threads
 def getBiz(bot, update):
     threadsIds = getBizThreads()
 
@@ -89,23 +93,26 @@ def getBiz(bot, update):
         bot.send_message(chat_id=chat_id, text=message)
 
 
-
+# sends the astrotools chart of the $ROT token
 def chart(bot, update):
     test = "https://app.astrotools.io/pair-explorer/0x5a265315520696299fa1ece0701c3a1ba961b888"
     chat_id = update.message.chat_id
     bot.send_message(chat_id=chat_id, text=test)
 
+# sends the astrotools chart of the $MAGGOT token
 def chartMaggot(bot, update):
     test = "https://app.astrotools.io/pair-explorer/0x46ba95ff4f4cd9353eadde43bee519fa50886e72"
     chat_id = update.message.chat_id
     bot.send_message(chat_id=chat_id, text=test)
 
+# uniswap page
 def getuniswap(bot, update):
     test = """<b>$ROT</b> -> https://app.uniswap.org/#/swap?inputCurrency=0xd04785c4d8195e4a54d9dec3a9043872875ae9e2
 <b>$MAGGOT</b> -> https://app.uniswap.org/#/swap?inputCurrency=0x163c754ef4d9c03fc7fa9cf6dd43bfc760e6ce89"""
     chat_id = update.message.chat_id
     bot.send_message(chat_id=chat_id, text=test, parse_mode='html')
 
+# tutorial on how to stake
 def stakeCommand(bot, update):
     text = """<b>How to stake/farm ROT :</b> 
 
@@ -123,6 +130,7 @@ See the full instructions on https://pastebin.pl/view/raw/3db66672"""
 
 last_time_since_check = 0
 
+# callback function that sends a reminder if no /biz/ threads are rooting
 def callback_4chan_thread(bot, job):
     biz = getBizThreads()
     if not biz:
