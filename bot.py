0a2514ddabf6f13ecbb142d66035460127d601be
rottedBen <71821951+rottedBen@users.noreply.github.com>
2020-09-28 10:14:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -412,7 +412,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('startBiz', callback_timer, pass_job_queue=True))
 
     job = updater.job_queue
-    job.run_repeating(check_new_proposal, 20, context=telegram_governance_url)
+    job.run_repeating(check_new_proposal, 20, context=rotten_main_chat_id)
 
     updater.start_polling()
     updater.idle()
