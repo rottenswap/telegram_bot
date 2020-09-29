a858d8afbb556a5e551356f0630dae4a18691f51
rottedBen <71821951+rottedBen@users.noreply.github.com>
2020-09-29 07:36:32 +0200

    Added grids to figure

--- bot.py
+++ bot.py
@@ -471,6 +471,7 @@ def print_chart(dates_raw, price):
     ax.yaxis.set_major_formatter('${x:1.3f}')
     ax.yaxis.tick_right()
     ax.yaxis.set_label_position("right")
+    ax.grid(alpha=0.2)
 
     plt.plot_date(dates, price, cb91_green)
     plt.gcf().autofmt_xdate()
