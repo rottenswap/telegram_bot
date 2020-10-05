9bfd9b8dc22546ff11399846de0b6fad5b3bb72d
rottedBen <71821951+rottedBen@users.noreply.github.com>
2020-10-05 22:32:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -557,6 +557,8 @@ def get_price_rot_raw():
 
     res_uni_query = graphql_client_uni.execute(query_uni_updated)
     json_resp_uni = json.loads(res_uni_query)
+    
+    pprint.pprint(json_resp_uni)
 
     rot_per_eth_7d = float(json_resp_uni['data']['t1']['derivedETH'])
     rot_per_eth_1d = float(json_resp_uni['data']['t2']['derivedETH'])
