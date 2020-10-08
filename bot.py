b5713291ffc94c70c93bba9e72412da08f3a243b
rottedBen <71821951+rottedBen@users.noreply.github.com>
2020-10-08 22:41:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -197,7 +197,7 @@ links = '<b>Website:</b> ' + create_href_str(url_website, 'rottenswap.org') + '\
         + '<b>Social medias: </b>' + create_href_str(url_twitter_rottenswap, 'Twitter') + ' ' + create_href_str(
     url_reddit_rottenswap, 'Reddit') + ' ' + create_href_str("https://discord.gg/WFp5sQ", 'Discord') + '\n' \
         + '<b>Merch: </b>' + create_href_str(url_merch_site1, 'RottenSwag') + ' ' + create_href_str(url_merch_site2, 'RottenMerch') + '\n' \
-        + '<b>Telegram groups:</b> @rottengovernance @rottenhelpgroup @RottenHalloween @RottenNFTs @ROTGamblingDapp 中國 -> @RottenSwapCN @RottenSwapAR <-- القناة العربية'
+        + '<b>Telegram groups:</b> @rottengovernance @rottenhelpgroup @RottenHalloween @RottenNFTs @ROTGamblingDapp 中國 -> @RottenSwapCN @RottenSwapAR - القناة العربية'
 
 # GIT INIT
 repo = Repo(MEME_GIT_REPO)
